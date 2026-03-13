1d862d969db46a05ccf29908211205c0717cef3e
Yasushi Itokazu <non.ya0910@gmail.com>
2026-03-13 16:57:13 +0900

    style: PC・タブレット閲覧時のUI崩れ防止（スマホサイズ固定レイアウトの実装）

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.6a4grkgtll8"
+    "revision": "0.5eahdttbm6o"
   }], {
     "ignoreURLParametersMatching": [/.*/]
   });
