86737b747aefa5000b2232360e7ba59c84eab70b
Yasushi Itokazu <non.ya0910@gmail.com>
2026-07-18 19:45:45 +0900

    chore: establish safe post-merge workspace

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+    port: 3040,
+    strictPort: true,
+  },
   plugins: [
     react(),
     VitePWA({
